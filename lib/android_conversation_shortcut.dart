ecd9f6761ab1b097ba41ca57d8373c13dfcd3a01
Ivan Chau <ivan.chau@tdam.com>
2022-10-28 23:04:09 -0400

    store image in file

--- lib/android_conversation_shortcut.dart
+++ lib/android_conversation_shortcut.dart
@@ -1,7 +1,9 @@
 import 'dart:async';
+import 'dart:io';
 
 import 'package:flutter/services.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:path_provider/path_provider.dart';
 
 class AndroidConversationShortcut {
   static const MethodChannel _channel =
@@ -15,12 +17,21 @@ class AndroidConversationShortcut {
     try {
       Uint8List? _iconBytes = person.icon?.data as Uint8List?;
 
+      String fileName = '${person.key}.png';
+
+      final directory = await getApplicationDocumentsDirectory();
+      String filePath = '${directory.path}/$fileName';
+
+      File file = File(filePath);
+      if (_iconBytes != null) {
+        file.writeAsBytesSync(_iconBytes);
+      }
+
       final String? shortcutID =
           await _channel.invokeMethod('createConversationShortcut', {
         'personName': person.name,
         'personKey': person.key,
-        'personIcon':
-            _iconBytes != null ? String.fromCharCodes(_iconBytes) : null,
+        'personIcon': file.path,
         'personBot': person.bot,
         'personImportant': person.important,
         'personUri': person.uri,
